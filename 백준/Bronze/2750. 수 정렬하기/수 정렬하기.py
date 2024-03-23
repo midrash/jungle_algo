1db0e0a43a7c381adc2cff79c6f2566f085454ed
parkjunghwan <midrash0740@gmail.com>
2024-03-23 19:52:52 +0900

    업데이트

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,6 +1,13 @@
-n = int(input())
-nums = [int(input()) for _ in range(n)]
-nums.sort()
+N = int(input())
 
-for i in range(len(nums)):
-    print(nums[i])
+arr = [int(input()) for _ in range(N)]
+
+min = 1001
+while len(arr) > 0:
+    for i, val in enumerate(arr):
+        if min > val:
+            min = val
+            check = i
+    print(arr[check])
+    del arr[check]
+    min = 1001
---
+++ 백준/test.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+arr = [int(input()) for _ in range(N)]
+
+min = 1000001
+while len(arr) > 0:
+    for i, val in enumerate(arr):
+        if min > val:
+            min = val
+            check = i
+    print(arr[check])
+    del arr[check]
+    min = 1000001
