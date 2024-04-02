674af5a6df9e1ce78ca2d7d1e7c05b3c3f84f887
ParkJungHwan <pjh0740@naver.com>
2024-04-02 20:23:53 +0900

    [Gold V] Title: 최소비용 구하기, Time: 316 ms, Memory: 57372 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,39 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+tree = [[] for _ in range(N + 1)]
+
+for i in range(M):
+    a, b, c = map(int, input().split())
+    tree[a].append((c, b))
+
+start, goal = map(int, input().split())
+
+cost = [sys.maxsize] * (N + 1)
+que = []
+cost[start] = 0
+heapq.heappush(que, (0, start))
+# print(f"큐 업데이트 : {que}")
+
+while que:
+    nowCost, startNode = heapq.heappop(que)
+    if cost[startNode] < nowCost:
+        continue
+    # print(f"출발지: {node}")
+    for toCost, toNode in tree[startNode]:
+        # print(f"확인 장소 : {toNode}, 비용 {toCost}")
+        # print(f"기존 비용 : {cost[toNode]}, 새 비용 {cost[node] + toCost}")
+        if cost[toNode] <= nowCost + toCost:
+            continue
+
+        cost[toNode] = nowCost + toCost
+        # print(f"비용 업데이트 : {cost}")
+        heapq.heappush(que, (nowCost + toCost, toNode))
+        # print(f"큐 업데이트 : {que}")
+
+print(f"{cost[goal]}")
+
