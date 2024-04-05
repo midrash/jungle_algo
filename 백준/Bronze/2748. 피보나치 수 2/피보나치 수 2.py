777524a22f31c543a70ce587b89397934cf7f388
ParkJungHwan <pjh0740@naver.com>
2024-04-05 16:37:24 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -8,6 +8,23 @@ n = int(input())
 
 fibo = [0, 1]
 
-for i in range(2, n + 1):
-    fibo.append(fibo[i - 1] + fibo[i - 2])
-print(fibo[n])
+# for i in range(2, n + 1):
+#     fibo.append(fibo[i - 1] + fibo[i - 2])
+# print(fibo[n])
+
+fibonachi = [-1] * (n + 1)
+
+
+def fibo(n):
+    if fibonachi[n] >= 0:
+        return fibonachi[n]
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        fibonachi[n] = fibo(n - 1) + fibo(n - 2)
+        return fibonachi[n]
+
+
+print(fibo(n))
