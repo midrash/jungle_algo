fcfefc83e71d417fb28559b98b254fad310e4f1f
ParkJungHwan <pjh0740@naver.com>
2024-04-27 10:38:41 +0900

    [Silver V] Title: 문서 검색, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -1,13 +1,7 @@
-doc = input()
+
+
 word = input()
+small = input()
+sp_word = word.split(small)
 
-start_index = 0
-count = 0
-while True:
-    find_index = doc.find(word, start_index)
-    if find_index < 0:
-        break
-    start_index = find_index + len(word)
-    count += 1
-
-print(count)
+print(len(sp_word) - 1)
