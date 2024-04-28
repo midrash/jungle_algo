0eedfb4a9a1fafe004daeb7367e3133dbe47f767
ParkJungHwan <pjh0740@naver.com>
2024-04-28 18:36:05 +0900

    [Silver III] Title: 두 수의 합, Time: 104 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -13,18 +13,7 @@ ri = len(arr) - 1
 while li < ri:
     if arr[li] + arr[ri] == x:
         cnt += 1
-        while (li + 1 <= ri) and (arr[li] == arr[li + 1]):
-            # print("여긴가?1")
-            cnt += 1
-            li += 1
-        while (ri - 1 >= li) and (arr[ri] == arr[ri - 1]):
-            # print("여긴가?1")
-            cnt += 1
-            ri -= 1
-        if (li + 1 <= ri) and arr[li] != arr[li + 1]:
-            li += 1
-        if (ri - 1 >= li) and arr[ri] != arr[ri - 1]:
-            ri -= 1
+        li += 1
     elif arr[li] + arr[ri] > x:
         ri -= 1
     else:
